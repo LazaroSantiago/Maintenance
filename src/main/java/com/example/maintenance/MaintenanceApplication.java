ab656aea47e8e8c335954c0e432c7ed422b8b0c1
kevinVigil <kevinvigil27@gmail.com>
2023-11-14 14:17:34 -0300

    testeo sin testear

--- src/main/java/com/example/maintenance/MaintenanceApplication.java
+++ src/main/java/com/example/maintenance/MaintenanceApplication.java
@@ -1,28 +1,19 @@
 package com.example.maintenance;
 
-import jakarta.annotation.PostConstruct;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import util.Test;
 
 @SpringBootApplication
-@ComponentScan({"util", "controller", "entity", "repository", "service"})
+@ComponentScan({"util", "controller", "entity", "repository", "service", "com.example.maintenance"})
 @EnableJpaRepositories("repository")
 @EntityScan("entity")
 public class MaintenanceApplication {
-    @Autowired
-    private Test test;
 
     public static void main(String[] args) {
         SpringApplication.run(MaintenanceApplication.class, args);
     }
 
-    @PostConstruct
-    public void init() {
-        test.test();
-    }
 }
